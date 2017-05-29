b78c9ad8714933165cdb218bb13cc6cc6973f9e0
Ripudaman <Rds0751@gmail.com>
2017-05-29 13:58:38 +0530

    first commit

---
+++ ApproApp/OffApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OffAppConfig(AppConfig):
+    name = 'OffApp'
---
+++ ApproApp/PeopApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PeopAppConfig(AppConfig):
+    name = 'PeopApp'
