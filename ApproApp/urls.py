437fdcf7182ccc06d4dcbfaf0baa57c06ad5c96f
rds0751 <rds0751@gmail.com>
2017-05-29 23:10:09 +0530

    Update urls.py

--- ApproApp/urls.py
+++ ApproApp/urls.py
@@ -6,16 +6,16 @@ from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
- admin.autodiscover()
+# admin.autodiscover()
 
 urlpatterns = [
     # Examples:
      url(r'^$', ApproApp.views.home, name='home'),
-     url(r'^ApproApp/', include('ApproApp.ApproApp.urls')),
+    # url(r'^ApproApp/', include('ApproApp.ApproApp.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
