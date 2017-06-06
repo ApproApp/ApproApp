6cbbc810e41fe13aaee275dab9a058afc1f82eb8
Ripudaman <Rds0751@gmail.com>
2017-06-06 10:06:18 +0530

    made better

--- manage.py
+++ manage.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 """
 Command-line utility for administrative tasks.
 """
 
-=======
->>>>>>> bd90123510bc5894ca7541d6577fcc23e9bc150d
+
 import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
+
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
         "ApproApp.settings"
     )
-=======
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
->>>>>>> bd90123510bc5894ca7541d6577fcc23e9bc150d
+
 
     from django.core.management import execute_from_command_line
 
