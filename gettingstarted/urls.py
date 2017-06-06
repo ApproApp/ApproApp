7adb94ca5f5c2f663259b0ca2f85370563b7e2b1
Ripudaman <Rds0751@gmail.com>
2017-06-06 11:12:54 +0530

    made better

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^$', hello.views.index, name='index'),
+   # url(r'^db', hello.views.db, name='db'),
+   # url(r'^admin/', include(admin.site.urls)),
 ]
