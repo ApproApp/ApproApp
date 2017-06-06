72fb8e8ab88d9adf5a31bf45319ef783f0d1907d
Ripudaman <Rds0751@gmail.com>
2017-06-06 11:17:47 +0530

    made better

--- ApproApp/urls.py
+++ ApproApp/urls.py
@@ -13,12 +13,12 @@ admin.autodiscover()
 
 urlpatterns = [
     # Examples:
-     url(r'^$', Core_views.home, name='home'),
+    url(r'^$', Core_views.home, name='home'),
     # url(r'^ApproApp/', include('ApproApp.ApproApp.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
